7bada977bcc17b3589eda7610062d017ab9d6401
Rafa Brown <shane.rafael@zohomail.com>
2021-11-28 17:30:08 -0700

    Add PATCH & POST routine functions

--- src/api/routines.js
+++ src/api/routines.js
@@ -29,3 +29,93 @@ export async function getRoutineById(id, token) {
     throw error.message;
   }
 }
+
+export async function createRoutine(token, name, goal, isPublic = false) {
+  try {
+    let headers = setHeaders(token);
+    const response = await axios.post(
+      `${BASE_URL}`,
+      {
+        name: name,
+        goal: goal,
+        isPublic: isPublic,
+      },
+      {
+        headers: headers,
+      }
+    );
+    const newRoutine = response.data;
+    return newRoutine;
+  } catch (err) {
+    console.error(err);
+    const error = err.response.data;
+    throw error.message;
+  }
+}
+
+export async function addActivityToRoutine(
+  token,
+  routineId,
+  activityId,
+  count,
+  duration
+) {
+  try {
+    let headers = setHeaders(token);
+    const response = await axios.post(
+      `${BASE_URL}/${routineId}/activities`,
+      {
+        activityId: activityId,
+        count: count,
+        duration: duration,
+      },
+      {
+        headers: headers,
+      }
+    );
+    const routineActivity = response.data;
+    return routineActivity;
+  } catch (err) {
+    console.error(err);
+    const error = err.response.data;
+    throw error.message;
+  }
+}
+
+export async function deleteRoutine(token, routineId) {
+  try {
+    let headers = setHeaders(token);
+    const response = await axios.delete(`${BASE_URL}/${routineId}`, {
+      headers: headers,
+    });
+    const data = response.data;
+    return data;
+  } catch (err) {
+    console.error(err);
+    const error = err.response.data;
+    throw error.message;
+  }
+}
+
+export async function updateRoutine(token, routineId, name, goal, isPublic) {
+  try {
+    let headers = setHeaders(token);
+    const response = await axios.patch(
+      `${BASE_URL}/${routineId}`,
+      {
+        name: name,
+        goal: goal,
+        isPublic: isPublic,
+      },
+      {
+        headers: headers,
+      }
+    );
+    const updatedRoutine = response.data;
+    return updatedRoutine;
+  } catch (err) {
+    console.error(err);
+    const error = err.response.data;
+    throw error.message;
+  }
+}
